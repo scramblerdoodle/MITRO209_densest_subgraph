a40c03fadd2e2f3051ce6adf4ccdc5f3ff5ca333
scramblerdoodle <orlando.pedro@outlook.com>
2021-01-11 14:01:46 +0100

    density fix, copy option, small comments

--- densest_graph.py
+++ densest_graph.py
@@ -9,13 +9,14 @@ class Graph():
     degrees = defaultdict(list)
     edges = {}
     nodes = {}
+    density = 0
     
     def __init__(self, data = {}):
         for k,v in data.items():
             # self.edges[k] = list(map(str, v))
             # interesting little tidbit: the whole thing breaks when we try to add self edges
             # this raises the question: to consider a graph to be dense, shall we only consider the outgoing edges,
-            # i.e. ignore the self-referencing ones?
+            # i.e. ignore the self-referencing ones? how about duplicated edges?
             self.edges[k] = list(filter(lambda x: x != k, map(str, v)))
 
 
@@ -25,12 +26,15 @@ class Graph():
             self.degrees[d].append(v)
             self.nodes[v] = d
 
+        self.density = self.avg_degree_density()
+
     def copy(self):
-        # This might have a really large complexity idk
+        # This technically has linear complexity O(V + E) but in the huge while loop for densest_subgraph it becomes quadratic
         H = Graph()
         H.edges     = deepcopy(self.edges)
         H.nodes     = deepcopy(self.nodes)
         H.degrees   = deepcopy(self.degrees)
+        H.density   = self.density
 
         return H
 
@@ -39,7 +43,7 @@ class Graph():
         return min(filter(lambda x: x[1], self.degrees.items()))
 
     def remove_node(self, v):
-        # DEBUG: print('v:', v, 'edges:', self.edges[v])
+        # print('v:', v, 'edges:', self.edges[v])
 
         for n in self.edges[v]:
             # removing v from the edges of each n
@@ -61,13 +65,14 @@ class Graph():
         # since we're removing edges and nodes one-by-one,
         # we could keep n_nodes and n_edges as attributes for the class itself and save ourselves some complexity
         n_nodes = len(self.edges) # O(V)
-        n_edges = sum( map( len, self.edges.values() ) ) # O(E)
+        n_edges = sum( map( len, self.edges.values() ) )/2 # O(E)
+        # gotta divide n_edges by 2 to account for the two-sided edges we create when reading the graph
         
         return n_edges / n_nodes if n_nodes else 0 # sanity check
 
 
 # algorithm itself:
-def densest_subgraph(G):
+def densest_subgraph(G, copy=False):
     H = G.copy()
 
     while G.edges: # i.e. while G isn't empty
@@ -78,10 +83,19 @@ def densest_subgraph(G):
         # print("min deg:", min_deg, "v:", v)
         
         G.remove_node(v) # remove v and its edges from G
-        ### TODO: THIS IS WHAT'S FUCKING UP THE PERFORMANCE
-        if G.avg_degree_density() > H.avg_degree_density():
-            # print(f"Density: {G.avg_degree_density()}")
-            H = G.copy()
+        G.density = G.avg_degree_density()
+        print(f"Density: {G.density}")
+        if G.density > H.density:
+            ### TODO: the deepcopy is what's destroying the performance
+            # it's actually linear, O(V + E), both spatial and algorithmically, BUT it's in a O(V + E) loop so... yeah
+            if copy: 
+                H = G.copy()
+            else:
+                # this is sort of a workaround but it's got a few issues - on the twitch db it misses out 3 nodes for some reason
+                # I have a feeling it's because it's not really keeping track of literally every step taken by the algo (removing nodes from G)
+                # so there may be a few nodes (namely when the density decreases) which aren't really removed from H and it gets confused
+                H.remove_node(v)
+                H.density = H.avg_degree_density()
 
     return H
 
@@ -98,7 +112,7 @@ def densest_subgraph(G):
 ### NOTE: twitch final density: 23.857142857142858
 
 if __name__ == "__main__":
-    ### READING INPUT FILE (json)
+    ### READING INPUT FILE
     args = sys.argv
 
     if len(args) != 2:
@@ -116,20 +130,23 @@ if __name__ == "__main__":
         for n, t in csvfile:
             data[n].append(t)
             data[t].append(n)
-    
-    print("Arranging data...")
-    graph = Graph(data)
-    
-    print("Looking for densest subgraph...")
-    
+
+
     import time
+    print("Looking for densest subgraph (w/o copy)...")    
     start = time.time()
-    H = densest_subgraph(graph)
+    H = densest_subgraph(Graph(data), copy=False)
     end = time.time()
 
-    print(H.edges)
-    print(H.nodes)
-    print("Elapsed time:", end - start)
+    print("\nLooking for densest subgraph (w/ copy)...")
+    start2 = time.time()
+    H2 = densest_subgraph(Graph(data), copy=True)
+    end2 = time.time()
+
+    # print(H.edges)
+    # print(H.nodes)
+    print("Elapsed time w/ copy:", end - start)
+    print("Elapsed time w/o copy:", end2 - start2)
 
 
     
